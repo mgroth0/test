1cfab5ddb8b08a7b9f35e0591ab382c84d8e6747
mgroth0 <mgroth49@gmail.com>
2022-10-10 21:20:19 -0400

    first commit

---
+++ src/commonMain/kotlin/matt/test/test.kt
@@ -0,0 +1,14 @@
+package matt.test
+
+import matt.lang.disabledButDefinitelyStillInByteCodeCode
+import org.junit.jupiter.api.extension.AfterTestExecutionCallback
+import kotlin.test.junit5.JUnit5Asserter
+
+fun yesIUseTestLibs() {
+  disabledButDefinitelyStillInByteCodeCode {
+	val yesIUseJUnitHereIsSomethingNotInlined = AfterTestExecutionCallback { TODO("Not yet implemented") }
+	val hereIsAnother = JUnit5Asserter
+	println(yesIUseJUnitHereIsSomethingNotInlined.afterTestExecution(null))
+	println(hereIsAnother.assertTrue({ "I'm so lazy" }, false))
+  }
+}
