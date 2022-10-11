e2f13a85ae56ff7c44328ebcd32e28c86355d9f5
mgroth0 <mgroth49@gmail.com>
2022-10-10 22:44:23 -0400

    autocommit

--- src/commonMain/kotlin/matt/test/test.kt
+++ src/commonMain/kotlin/matt/test/test.kt
@@ -1,10 +1,12 @@
 package matt.test
 
+import matt.lang.YesIUseLang
 import matt.lang.disabledButDefinitelyStillInByteCodeCode
 import org.junit.jupiter.api.extension.AfterTestExecutionCallback
 import kotlin.test.junit5.JUnit5Asserter
 
 fun yesIUseTestLibs() {
+  YesIUseLang
   disabledButDefinitelyStillInByteCodeCode {
 	val yesIUseJUnitHereIsSomethingNotInlined = AfterTestExecutionCallback { TODO("Not yet implemented") }
 	val hereIsAnother = JUnit5Asserter
