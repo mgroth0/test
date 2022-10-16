07e6b86c82e3a65d87d2d98df20d6e29f9515d96
mgroth0 <mgroth49@gmail.com>
2022-10-16 16:29:05 -0400

    autocommit

--- src/commonMain/kotlin/matt/test/test.kt
+++ src/commonMain/kotlin/matt/test/test.kt
@@ -2,9 +2,10 @@ package matt.test
 
 import matt.lang.YesIUseLang
 import matt.lang.disabledButDefinitelyStillInByteCodeCode
-import org.junit.jupiter.api.extension.AfterTestExecutionCallback
-import kotlin.test.junit5.JUnit5Asserter
+//import org.junit.jupiter.api.extension.AfterTestExecutionCallback
+//import kotlin.test.junit5.JUnit5Asserter
 
+/*
 fun yesIUseTestLibs() {
   YesIUseLang
   disabledButDefinitelyStillInByteCodeCode {
@@ -13,4 +14,4 @@ fun yesIUseTestLibs() {
 	println(yesIUseJUnitHereIsSomethingNotInlined.afterTestExecution(null))
 	println(hereIsAnother.assertTrue({ "I'm so lazy" }, false))
   }
-}
+}*/
