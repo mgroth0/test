05339c2c6de339f3c2529f7f9a67f97651a550c9
mgroth0 <mgroth49@gmail.com>
2022-10-26 19:47:20 -0400

    autocommit

--- src/commonMain/kotlin/matt/test/test.kt
+++ src/commonMain/kotlin/matt/test/test.kt
@@ -1,16 +1,2 @@
 package matt.test
 
-import matt.lang.YesIUseLang
-import matt.lang.disabledButDefinitelyStillInByteCodeCode
-import org.junit.jupiter.api.extension.AfterTestExecutionCallback
-import kotlin.test.junit5.JUnit5Asserter
-
-fun yesIUseTestLibs() {
-  YesIUseLang
-  disabledButDefinitelyStillInByteCodeCode {
-	val yesIUseJUnitHereIsSomethingNotInlined = AfterTestExecutionCallback { TODO("Not yet implemented") }
-	val hereIsAnother = JUnit5Asserter
-	println(yesIUseJUnitHereIsSomethingNotInlined.afterTestExecution(null))
-	println(hereIsAnother.assertTrue({ "I'm so lazy" }, false))
-  }
-}
---
+++ src/jvmMain/kotlin/matt/test/test.kt
@@ -0,0 +1,18 @@
+package matt.test
+
+import matt.lang.YesIUseLang
+import matt.lang.disabledButDefinitelyStillInByteCodeCode
+import org.junit.jupiter.api.extension.AfterTestExecutionCallback
+import kotlin.test.junit5.JUnit5Asserter
+
+
+
+fun yesIUseTestLibs() {
+  YesIUseLang
+  disabledButDefinitelyStillInByteCodeCode {
+	val yesIUseJUnitHereIsSomethingNotInlined = AfterTestExecutionCallback { TODO("Not yet implemented") }
+	val hereIsAnother = JUnit5Asserter
+	println(yesIUseJUnitHereIsSomethingNotInlined.afterTestExecution(null))
+	println(hereIsAnother.assertTrue({ "I'm so lazy" }, false))
+  }
+}
