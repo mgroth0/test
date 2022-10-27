da9d8a8f3faf8904d176b883ca82091e562dbee3
mgroth0 <mgroth49@gmail.com>
2022-10-26 20:02:21 -0400

    autocommit

--- src/commonMain/kotlin/matt/test/test.kt
+++ src/commonMain/kotlin/matt/test/test.kt
@@ -1,2 +1,18 @@
 package matt.test
 
+/*https://stackoverflow.com/questions/30445974/kotlin-java-lang-nosuchmethoderror-in-tests*/
+fun reportAndReThrowErrors(op: ()->Unit) {
+  try {
+	op()
+  } catch (throwable: Throwable) {
+	var e: Throwable? = throwable
+	do {
+	  e!!
+	  println(e)
+	  e.printStackTrace()
+	  e = e.cause
+	} while (e != null)
+	throw throwable
+  }
+
+}
