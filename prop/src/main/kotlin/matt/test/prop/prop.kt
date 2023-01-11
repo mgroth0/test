fb424d878f2939aa4190792c5abbe818402a7556
mgroth0 <mgroth49@gmail.com>
2023-01-11 14:52:23 -0500

    autocommit

--- prop/src/main/kotlin/matt/test/prop/prop.kt
+++ prop/src/main/kotlin/matt/test/prop/prop.kt
@@ -1,6 +1,5 @@
-
 package matt.test.prop
 
 
-
-val DO_MANUAL_TESTS = "DO_MANUAL_TESTS"
+/*not making this a const as a shortcut so my dep analyzer doesnt flag this library as unused*/
+val DO_MANUAL_TESTS get() = "DO_MANUAL_TESTS"
